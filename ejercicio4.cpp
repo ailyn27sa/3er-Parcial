ab24b559606c0cd84e670b6cf47b093ff6eab248
Santacruz Avalos Ailyn <168689298+ailyn27sa@users.noreply.github.com>
2024-05-22 22:45:16 -0600

    Add files via upload

---
+++ ejercicio4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char primero, ultimo;
+    cout<<" Introduzca su primera y ultima inicial ";
+    cin>>primero>>ultimo;
+    cout<<" Hola "<<primero<<ultimo;
+    return 0;
+}
