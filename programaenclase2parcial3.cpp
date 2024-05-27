6303088c5b41438f218a2956e614431b57f04bd0
Santacruz Avalos Ailyn <168689298+ailyn27sa@users.noreply.github.com>
2024-05-27 17:19:24 -0600

    Add files via upload

---
+++ programaenclase2parcial3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    srand(time(0)); 
+    int matriz[3][3];
+
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+           
+            matriz[i][j] = rand() % 10;
+        }
+    }
+
+   
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
