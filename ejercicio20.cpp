46e84b4f8b4f0c2207b2b1bafa501a7fb94ea51a
Santacruz Avalos Ailyn <168689298+ailyn27sa@users.noreply.github.com>
2024-06-02 10:15:05 -0600

    Add files via upload

---
+++ ejercicio20.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double numero = 1;
+
+    while(numero <= 100) {
+        cout << numero << endl;
+        numero += 2;
+    }
+
+    return 0;
+}
